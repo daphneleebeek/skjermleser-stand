1a3684a2d5466ab5b9a3d1ef7c2e0087663918b8
Daphne Leebeek <daphne.leebeek@nav.no>
2021-10-13 21:03:14 +0200

    Lag skjemaelementer i random rekkefølge

--- src/components/Skjema.jsx
+++ src/components/Skjema.jsx
@@ -1,6 +1,28 @@
 import React from 'react';
+import styled from "styled-components";
+
+const Input = ({label, id}) => {
+    return (
+        <>
+            <label htmlFor={id}>{label}:</label>
+            <input type="text" id={id} />
+        </>
+    )
+}
+
+const StyledForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  width: 50%;
+`;
 
 const Skjema = ({ settStartetSkjema }) => {
+
+    const skjemaKomponenter = [
+        <Input key={'navn'} label={'Navnet ditt'} id={'navn'} />,
+        <Input key={'tlf'} label={'Telefonnr'} id={'tlf'} />,
+        <Input key={'noeannet'} label={'Noe annet'} id={'noeannet'} />
+    ]
 
     const sendInnSkjema = (event) => {
         event.preventDefault();
@@ -8,11 +30,10 @@ const Skjema = ({ settStartetSkjema }) => {
     }
 
     return (
-        <form onSubmit={sendInnSkjema}>
-            <label htmlFor="navn">Navnet ditt:</label>
-            <input type="text" id="navn" name="navn" />
+        <StyledForm onSubmit={sendInnSkjema}>
+            {skjemaKomponenter.sort((a, b) => 0.5 - Math.random())}
             <button type="submit">Send inn</button>
-        </form>
+        </StyledForm>
     );
 };
 
