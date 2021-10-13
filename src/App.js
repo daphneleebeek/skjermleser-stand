d1adced2b716265ff9bbde12e629cd21aa619f8a
Daphne Leebeek <daphne.leebeek@nav.no>
2021-10-13 20:13:18 +0200

    create-react-app

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <main className="App">
+      <header className="App-header">
+        <h1>Skjermeleser-stand</h1>
+      </header>
+    </main>
+  );
+}
+
+export default App;
